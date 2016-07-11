d1098e7947875c08983951dc96f4fe55a7e4098f
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-07-11 19:56:18 +0700

    Search Delete

--- js/lib/selector/plugins/datatable/selector-datatable.js
+++ js/lib/selector/plugins/datatable/selector-datatable.js
@@ -20,25 +20,22 @@
         columnDefs = datatable._dt_setting.columnDefs;
         thead.empty();
         tbody.empty();
-
         columnDefs.forEach(function(values){
-            //console.log(values.name);
-            var tr = document.createElement('tr'),
-            th;
-            var arr = Object.keys(values).map(function (key) {return values[key]});
-            arr.forEach(function(value){
-                th = document.createElement('th');
-                th.innerHTML = value;
-                tr.appendChild(th);
-                datatable._dt_setting.columnRenderedCallback.call(datatable, tr, th, value);
-            });
-            thead.append(tr);
-            datatable._dt_setting.rowRenderedCallback.call(datatable, tr, values);
+            var th = document.createElement('th');
+            if (values.name == 'sex' || values.name == 'maritalStatus' 
+                ||  values.name == 'dob' || values.name == 'address' 
+                || values.name == 'email')
+            {
+                th.className = "md-hidden sm-hidden xs-hidden";
+            }
+            
+            th.innerHTML = values.name;
+            thead.append(th);
         });
 
         //check if data is an array or an object
         //if object then map to array
-        if(!Array.isArray(data[0])){
+        if (!Array.isArray(data[0])) {
             data = data.map(function(obj) { 
                 return [obj.id, obj.name, 
                 obj.sex, obj.maritalStatus,
@@ -47,14 +44,15 @@
         }
 
         data.forEach(function(values) {
-            console.log(values);
             var tr = document.createElement('tr'),
-            td;
+            td,
+            dataReturn = values;
             values.forEach(function(value) {
                 td = document.createElement('td');
+                td.className = "md-hidden sm-hidden xs-hidden";
                 td.innerHTML = value;
                 tr.appendChild(td);
-                datatable._dt_setting.columnRenderedCallback.call(datatable, tr, td, value);
+                datatable._dt_setting.columnRenderedCallback.call(datatable, tr, td, dataReturn);
             });
             tbody.append(tr);
             datatable._dt_setting.rowRenderedCallback.call(datatable, tr, values);
--- js/main.js
+++ js/main.js
@@ -1,45 +1,79 @@
-(function ($) {
-     var data = [
-        {
-            id: 10000,
-            name: "Jole Osteen",
-            sex: "M",
-            maritalStatus: "M",
-            dob:"01-01-1987",
-            title:"Senior Software Engineer",
-            address:"1355 Market Street, Suite 900 San Francisco, CA 94103",
-            phoneNumber:"(123) 456-7890",
-            email: "jole.osteen@example.com"
-        }
-    ];
-   
-    $('.datatable').dataTable({
-        data: data,
-        columnDefs:[
-        { name: 'id' },
-        { name: 'name' },
-        { name: 'sex' },
-        { name: 'maritalStatus' },
-        { name: 'dob' },
-        { name: 'title' },
-        { name: 'address' },
-        { name: 'phoneNumber' },
-        { name: 'email' }
-        ],
-        rowRenderedCallback: function(tr, data){
-            var td = document.createElement('td'),
-            delBtn = document.createElement('i');
+(function($) {
+    var data = [{
+        id: 10000,
+        name: "Jole Osteen",
+        sex: "M",
+        maritalStatus: "M",
+        dob: "01-01-1987",
+        title: "Senior Software Engineer",
+        address: "1355 Market Street, Suite 900 San Francisco, CA 94103",
+        phoneNumber: "(123) 456-7890",
+        email: "jole.osteen@example.com"
+    },
+    {
+        id: 10000,
+        name: "Jole Osteen",
+        sex: "M",
+        maritalStatus: "M",
+        dob: "01-01-1987",
+        title: "Senior Software Engineer",
+        address: "1355 Market Street, Suite 900 San Francisco, CA 94103",
+        phoneNumber: "(123) 456-7890",
+        email: "jole.osteen@example.com"
+    },
+    {
+        id: 10000,
+        name: "Phuc",
+        sex: "M",
+        maritalStatus: "M",
+        dob: "01-01-1987",
+        title: "Senior Software Engineer",
+        address: "1355 Market Street, Suite 900 San Francisco, CA 94103",
+        phoneNumber: "(123) 456-7890",
+        email: "jole.osteen@example.com"
+    }];
 
-            delBtn.className = 'fa fa-trash red';
-            delBtn.addEventListener("click", function(){
-                confirm("Are you sure you want to delete #" + data[0]);
-            });
-            td.appendChild(delBtn);
-            tr.appendChild(td);
+$('.datatable').dataTable({
+    data: data,
+    columnDefs: [
+        {name: 'id'}, 
+        {name: 'name'},
+        {name: 'sex'}, 
+        {name: 'maritalStatus'}, 
+        {name: 'dob'}, 
+        {name: 'title'}, 
+        {name: 'address'}, 
+        {name: 'phoneNumber'},
+        {name: 'email'}, 
+        {name: ''}
+    ],
+    searchable: true,
+    pageSize: 2,
+    columnRenderedCallback: function(tr, td, data) {
+        if(data[2] == 'M'){
+            data[2] = 'Male';
         }
+        if(data[3] == 'M'){
+            data[3] = 'Married';
+        }
+        td.className = "md-hidden sm-hidden xs-hidden"; 
+    },
+    rowRenderedCallback: function(tr, data) {
+        var td = document.createElement('td'),
+        delBtn = document.createElement('i');
+
+        delBtn.className = 'fa fa-trash red';
+        delBtn.addEventListener("click", function() {
+            confirm("Are you sure you want to delete #" + data[0]);
+            var row = this.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        });
+        td.appendChild(delBtn);
+        tr.appendChild(td);
+    }
     });
 })(Selector);
 
-function dropDown(){
+function dropDown() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
 }
